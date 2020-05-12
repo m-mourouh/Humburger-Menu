fb9746156477e758506fd01de9fee8199cd680a6
Deltaty Code <60442896+Deltaty-Code@users.noreply.github.com>
2020-05-12 18:57:10 +0000

    Add files via upload

---
+++ app.js
@@ -0,0 +1,17 @@
+// const mainMenu = document.querySelector('.mainMenu');
+// const closeMenu = document.querySelector('.closeMenu');
+// const openMenu = document.querySelector('.openMenu');
+
+
+
+
+// openMenu.addEventListener('click',show);
+// closeMenu.addEventListener('click',close);
+
+// function show(){
+//     mainMenu.style.display = 'flex';
+//     mainMenu.style.top = '0';
+// }
+// function close(){
+//     mainMenu.style.top = '-100%';
+// }
