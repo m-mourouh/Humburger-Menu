70eb10741e286b5e3a8ac2351c620d716dff971a
MOHAMED MOUROUH <60442896+Deltaty-Code@users.noreply.github.com>
2020-11-20 15:27:03 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
-// const mainMenu = document.querySelector('.mainMenu');
-// const closeMenu = document.querySelector('.closeMenu');
-// const openMenu = document.querySelector('.openMenu');
+const mainMenu = document.querySelector('.mainMenu');
+const closeMenu = document.querySelector('.closeMenu');
+const openMenu = document.querySelector('.openMenu');
 
 
 
 
-// openMenu.addEventListener('click',show);
-// closeMenu.addEventListener('click',close);
+openMenu.addEventListener('click',show);
+closeMenu.addEventListener('click',close);
 
-// function show(){
-//     mainMenu.style.display = 'flex';
-//     mainMenu.style.top = '0';
-// }
-// function close(){
-//     mainMenu.style.top = '-100%';
-// }
+function show(){
+    mainMenu.style.display = 'flex';
+    mainMenu.style.top = '0';
+}
+function close(){
+    mainMenu.style.top = '-100%';
+}
